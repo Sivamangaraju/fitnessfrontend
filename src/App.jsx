eb5f5cf35c855086840719d58d3509a7eee09eca
Sivamangaraju <sivamangarajuboramchu@gmail.com>
2024-11-04 19:00:53 +0530

    fitness-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { ThemeProvider, styled } from "styled-components";
-import { lightTheme,darkTheme } from "./Utils/Theme";
+import { lightTheme, darkTheme } from "./Utils/Theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Authentication from "./Pages/Authentication";
-import { useState, useEffect, useContext} from "react";
+import { useState, useEffect, useContext } from "react";
 import { useSelector } from "react-redux";
 import Navbar from "./Components/Navbar";
 import Dashboard from "./Pages/Dashboard";
-import History from "./pages/History/History";
+import History from "./pages/History/History.jsx";
 import Tutorials from "./Pages/Tutorials/Tutorials";
 import LaunchWorkout from "./Pages/LaunchWorkout/LaunchWorkout";
 import Bmi from "./Components/BMI/Bmi";
@@ -14,18 +14,20 @@ import Contact from "./Pages/Contact/Contact";
 import SplashScreen from "./Components/SplashScreen/SplashScreen";
 import ForgotForm from "./Components/ForgotPassword/ForgotForm";
 import Dropdowns from "./Components/Cards/Dropdowns";
-import {ThemeContext} from './Utils/ThemeContext';
-import BackImage from './Components/Assets/dark.jpg';
+import { ThemeContext } from "./Utils/ThemeContext";
+import BackImage from "./Components/Assets/dark.jpg";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
-  background-image: url(${({ theme }) => (theme.theme==='true'?BackImage:'')});
-  background-color:${({ theme }) => (theme.theme==='true'?'none':'#f3efe9f5')};
+  background-image: url(${({ theme }) =>
+    theme.theme === "true" ? BackImage : ""});
+  background-color: ${({ theme }) =>
+    theme.theme === "true" ? "none" : "#f3efe9f5"};
   background-repeat: no-repeat;
-  background-size:cover;
+  background-size: cover;
   background-position: center;
   color: ${({ theme }) => theme.text_primary};
   overflow-x: hidden;
@@ -34,7 +36,6 @@ const Container = styled.div`
 `;
 
 function App() {
-
   const { currentUser } = useSelector((state) => state.user);
   const [showSplash, setShowSplash] = useState(false);
   const { themeColor, setThemeColor } = useContext(ThemeContext);
@@ -49,23 +50,34 @@ function App() {
   }, [currentUser]);
 
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", themeColor ? "dark" : "light");
+    document.documentElement.setAttribute(
+      "data-theme",
+      themeColor ? "dark" : "light"
+    );
   }, [themeColor]);
 
   return (
-    <ThemeProvider theme={themeColor? darkTheme:lightTheme}>
+    <ThemeProvider theme={themeColor ? darkTheme : lightTheme}>
       <BrowserRouter>
         {currentUser ? (
           showSplash ? (
             <SplashScreen />
           ) : (
-            <Container >
+            <Container>
               <Navbar currentUser={currentUser} />
               <Routes>
-                <Route path="/" exact element={<Dashboard currentUser={currentUser} />} />
+                <Route
+                  path="/"
+                  exact
+                  element={<Dashboard currentUser={currentUser} />}
+                />
                 <Route path="/history" exact element={<History />} />
                 <Route path="/tutorials" exact element={<Tutorials />} />
-                <Route path="/launchworkout" exact element={<LaunchWorkout />} />
+                <Route
+                  path="/launchworkout"
+                  exact
+                  element={<LaunchWorkout />}
+                />
                 <Route path="/bmi" exact element={<Bmi />} />
                 <Route path="/contact" exact element={<Contact />} />
                 <Route path="/dropdown" exact element={<Dropdowns />} />
@@ -85,4 +97,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
